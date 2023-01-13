4d0feac50905a9f20ec18ebed38f20d93ca274a5
esra tansuk <esra.balkis@outlook.com>
2023-01-13 12:13:04 +0300

    Events completed

--- src/app.js
+++ src/app.js
@@ -1,39 +1,19 @@
-// var pName = "IPhone 14 Pro "
-// var pPrice = 50000
-
-// var product = {
-//   name: "IPhone 15",
-//   price: 40000,
-//   description: "iyi telefon",
-//   colors: ["silver", "gold", "red"],
-// };
-
 var products = [
   {
     name: "IPhone 15",
     price: 40000,
-    description: "iyi telefon",
-    colors: ["silver", "gold", "red"],
   },
 
   {
     name: "IPhone 14",
     price: 60000,
-    description: "iyi telefon",
-    colors: ["white", "blue", "red"],
   },
 
   {
     name: "IPhone 17",
     price: 80000,
-    description: "iyi telefon",
-    colors: ["white", "blue", "red"],
   },
 ];
-
-function formatPrice(product) {
-  return <p>{product.price} TL</p>;
-}
 
 // function printDescription(product) {
 //   if (product.description) {
@@ -41,30 +21,25 @@ function formatPrice(product) {
 //   }
 // }
 
+function addProduct(event, pName) {
+  console.log(event.target, pName);
+}
+
 var template = (
   <div>
     <h1 id="header">Ürün Listesi</h1>
 
-    {products.map((product,index) => (
+    {products.map((product, index) => (
       <div className="product-details" key={index}>
-        {product.name && product.name.length > 3 ? (
-          <h2> {product.name} </h2>
-        ) : (
-          <p>ürün ismi girilmemiştir</p>
-        )}
-        {/* <p>{formatPrice(product)}</p> */}
-        {product.price && product.price > 0 && formatPrice(product)}
-        {/* { printDescription(product) } */}
-        {/* product description varsa && */}
-        {product.description && <p>{product.description}</p>}
-        {product.colors.length > 0 ? <p>renk seçeneklerimiz mevcut</p> : ""}
-
-        <ul>
-          {/* color map içine gelecek her bir değerin ismi  */}
-          {product.colors.map((color, index) => (
-            <li key={index}> {color}</li>
-          ))}
-        </ul>
+        {<h2>{product.name}</h2>}
+        {product.price}
+        <button
+          type="button"
+          id={index}
+          onClick={(event) => addProduct(event, product.name)}
+        >
+          Ekle
+        </button>
       </div>
     ))}
   </div>
